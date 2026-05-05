c16a27f1698ec9b458e5a3e5aa7e71ffe018c466
raushan331 <raushankumar@Raushan-Kumar.local>
2026-05-05 14:11:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TicTac } from "./Component/TicTacToe/TicTac.jsx";
+
+function App() {
+  return (
+    <div>
+      <TicTac />
+    </div>
+  );
+}
+
+export default App;
